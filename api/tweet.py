377dcb441cde6da3b4676a0fa1fc0c4d725ee998
Gery THRASIBULE <g.thrasibule@lecomptoirdespharmacies.fr>
2020-03-18 17:01:39 -0400

    Search : add search with since
    Signed-off-by: Gery THRASIBULE <g.thrasibule@lecomptoirdespharmacies.fr>

--- api/tweet.py
+++ api/tweet.py
@@ -12,6 +12,14 @@ async def search(request):
     return JSONResponse(list(map(tweet_mapper.to_json, tweets)))
 
 
+async def since_search(request):
+    query = request.query_params['q']
+    last_retweet_id = config_service.get_last_retweet_id()
+    cursor = tweet_service.simple_search(query, last_retweet_id)
+    tweets = tweet_mapper.flatten_cursor(cursor)
+    return JSONResponse(list(map(tweet_mapper.to_json, tweets)))
+
+
 async def initialise_job(request):
     query = request.query_params['q']
     cursor = tweet_service.simple_search(query)
